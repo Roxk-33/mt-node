8f1829a485ba714c4ee0573ed51d0866ea3add50
Roxk-33 <xiaorock@126.com>
2019-02-26 20:39:11 +0800

    bugfix

--- app/router/merchant.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = app => {
-  const { router, controller, middleware } = app;
-  // 商家相关
-  router.post('/v1/merchant/register', controller.merchant.register); //-注册
-};
